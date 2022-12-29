dd21f74030d050a48aebb5a00d3f923cf9688180
amr <aa5473@fayoum.edu.eg>
2022-12-29 22:19:14 +0300

    i am about daring

---
+++ src/views/SinUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <SinUpForm />
+  </div>
+</template>
+
+<script>
+import SinUpForm from "@/components/SinUp.vue";
+export default {
+  name: "SinUp",
+  components: {
+    SinUpForm,
+  },
+};
+</script>
