b652de3589e659c891a99318304dae2492feba25
amr <aa5473@fayoum.edu.eg>
2022-12-30 20:43:11 +0300

    all praises is to Allah

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "normalize.css";
+
 createApp(App).use(store).use(router).mount("#app");
